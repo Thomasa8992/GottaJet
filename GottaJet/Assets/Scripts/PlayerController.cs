fc0ee1159a6566ea95ccf5ab893ddedc002a7222
Adrian Thomas <thomasa8992@hotmail.com>
2020-03-22 21:13:22 -0500

    Added a player controller script along with logic that controls the player movement and player boundaries on screen

---
+++ GottaJet/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MovementSpeed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        handlePlayerMovement();
+
+        handlePlayerBoundaries();
+    }
+
+    private void handlePlayerBoundaries() {
+        handlePlayerLeftBoundary();
+        handlePlayerRightBoundary();
+        handlePlayerTopBoundary();
+        handlePlayerBottomBoundary();
+    }
+
+    private void handlePlayerLeftBoundary() {
+        var leftBoundary = -22;
+        var playerHasReachLeftBoundary = transform.position.z < leftBoundary;
+
+        if (playerHasReachLeftBoundary) {
+            transform.position = new Vector3(transform.position.x, transform.position.y, leftBoundary);
+        }
+    }
+
+    private void handlePlayerRightBoundary() {
+        var rightBoundary = 22;
+        var playerHasReachedRightBoundary = transform.position.z > rightBoundary;
+
+        if (playerHasReachedRightBoundary) {
+            transform.position = new Vector3(transform.position.x, transform.position.y, rightBoundary);
+        }
+    }
+
+    private void handlePlayerTopBoundary() {
+        var TopBoundary = 7;
+        var playerHasReachedTopBoundary = transform.position.y > TopBoundary;
+
+        if (playerHasReachedTopBoundary) {
+            transform.position = new Vector3(transform.position.x, TopBoundary, transform.position.z);
+        }
+    }
+
+    private void handlePlayerBottomBoundary() {
+        var BottomBoundary = -8;
+        var playerHasReachedBottomBoundary = transform.position.y < BottomBoundary;
+
+        if (playerHasReachedBottomBoundary) {
+            transform.position = new Vector3(transform.position.x, BottomBoundary, transform.position.z);
+        }
+    }
+
+    private void handlePlayerMovement() {
+        handleVerticalMovementByPlayerInput();
+        handleHorizontalMovementByPlayerInput();
+    }
+
+    private void handleHorizontalMovementByPlayerInput() {
+        var horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * MovementSpeed * horizontalInput);
+    }
+
+    private void handleVerticalMovementByPlayerInput() {
+        var verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.up * Time.deltaTime * MovementSpeed * verticalInput);
+    }
+}
