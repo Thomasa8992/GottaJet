f78a3d66cc373793dee23d3a6a0d0d661792cd13
Adrian Thomas <thomasa8992@hotmail.com>
2020-03-23 21:35:18 -0500

    Added EnemyController and EnemySpawnManager along with logic to make the enemy prefab shoot projectiles and logic that spawns enemy prefabs

---
+++ GottaJet/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public GameObject projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("ShootProjectile", 2, 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ShootProjectile() {
+        Instantiate(projectile, transform.position, projectile.transform.rotation);
+    }
+}
---
+++ GottaJet/Assets/Scripts/EnemySpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawnRangeY = 8;
+    private float spawnPositionZ = 20;
+    public int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomEnemy", 2, 1.5f);
+        SpawnRandomEnemy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SpawnRandomEnemy() {
+        var spawnPosition = new Vector3(0, Random.Range(-spawnRangeY, spawnRangeY), 26);
+
+        if(counter <= 1) {
+            counter++;
+            Instantiate(enemyPrefab, spawnPosition, enemyPrefab.transform.rotation);
+        }
+    }
+}
