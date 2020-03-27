944430cf182b95368cea7e0a706376c8d6f0712f
Adrian Thomas <thomasa8992@hotmail.com>
2020-03-26 23:18:35 -0500

    Added script to add items that randomly spawn from the top, added scirpt for the fuel item and the debris item

---
+++ GottaJet/Assets/Scripts/ItemSpawnManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawnManager : MonoBehaviour
+{
+    public GameObject[] items;
+    public float spawnRangeZ = 25;
+    public float spawnPositionY = 8;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomItem", 10, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SpawnRandomItem() {
+        var spawnPosition = new Vector3(0, spawnPositionY, Random.Range(-spawnRangeZ, spawnRangeZ));
+        var itemIndex = Random.Range(0, items.Length);
+
+        Instantiate(items[itemIndex], spawnPosition, items[itemIndex].transform.rotation);
+    }
+}
